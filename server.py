f8f49178a83f29d26f8ec8ccb14f51ce416ef846
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-27 17:59:02 -0700

    Completed part 2

--- server.py
+++ server.py
@@ -25,10 +25,12 @@ def index():
     
     return render_template("homepage.html")
 
+
 @app.route("/users")
 def user_list():
     """Show list of users."""
 
+    #make a query to get all users
     users = User.query.all()
     return render_template("user_list.html", users=users)
 
@@ -36,6 +38,7 @@ def user_list():
 def show_user(user_id):
     """Show information of the chosen user."""
 
+    #get the first user with a user_id
     user = User.query.filter_by(user_id=user_id).first()
    
     return render_template("user_info.html", user=user)
@@ -44,17 +47,52 @@ def show_user(user_id):
 def movie_list():
     """Show list of movies."""
 
+    #make a query to get a list of all movies
     movies = Movie.query.order_by("title").all()
     return render_template("movie_list.html", movies=movies)
 
-@app.route("/movies/<movie_title>")
-def show_movie(movie_title):
+@app.route("/movies/<int:movie_id>", methods=["GET"])
+def show_movie(movie_id):
     """Show information of the chosen movie."""
 
-    movie = Movie.query.filter_by(title=movie_title).first()
+    #getting the first movie object with a movie_id
+    movie = Movie.query.filter_by(movie_id=movie_id).first()
    
     return render_template("movie_info.html", movie=movie)
 
+@app.route("/movies/<int:movie_id>", methods=['POST'])
+def rate_process(movie_id):
+    """Action for processing movie rating."""
+
+    #get a rating from user input
+    rating = request.form.get('rating')
+
+    #get a user email from the current session
+    user_email = session["current_user"]
+    
+    # get a current user object by the user email
+    current_user_info = User.query.filter_by(email = user_email).first()
+    #get a current user id
+    current_user_id = current_user_info.user_id
+
+    #get a current user rating.
+    current_rating = Rating.query.filter_by(movie_id = movie_id, user_id = current_user_id).first()
+
+    #if current user has no rating to the current movie
+    #add a new rating instance
+    #if not - update the current rating that the user has for the movie
+    if current_rating is None:
+        new_rating = Rating(movie_id=movie_id,
+                            user_id=current_user_id, 
+                            score = rating)
+        db.session.add(new_rating)
+    else: 
+        current_rating.score = rating
+    
+    db.session.commit()
+
+    flash("You rated successfully!!!")
+    return redirect("/")
 
 
 @app.route("/register", methods=['GET'])
@@ -62,7 +100,6 @@ def register_form():
     """Show page for the register form."""
 
     return render_template("register_form.html")
-
 
 
 @app.route("/register", methods=['POST'])
@@ -74,6 +111,7 @@ def register_process():
     age = request.form.get('age')
     zipcode = request.form.get('zipcode')
 
+    #create a new_user instane of the User class, add it to database
     new_user = User(email=email, 
                     password=password, 
                     age=age, 
@@ -102,8 +140,12 @@ def login_process():
     email = request.form.get('email')
     password = request.form.get('password')
 
+    #get the current_user by email
     current_user = User.query.filter_by(email = email).first()
 
+    #check if we have current_user, we validate the password
+    #return to user_info page if password is correct
+    #if not, flash message 
     if current_user:
         if current_user.password == password:
             session["current_user"] = email
